a1cec5d24ba930695e80067d6897f1ed45b71cae
jcamara <camarajoao1@gmail.com>
2023-02-14 19:06:20 -0800

    create functions and for loop to populate shows section with content

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -34,4 +34,68 @@ const shows = [
         venue: "Press Club",
         location: "San Francisco, CA",
     }
-]
+];
+
+const schedule = document.querySelector(".schedule");
+
+for (let show of shows) {
+    const concert = document.createElement("div");
+    concert.classList.add('schedule__concerts');
+
+    const keyDate = document.createElement("p");
+    keyDate.classList.add('schedule__key');
+    keyDate.innerText = 'DATE';
+    concert.appendChild(keyDate);
+
+    const valueDate = document.createElement("p");
+    valueDate.classList.add('schedule__value', 'schedule__value--bold');
+    valueDate.innerText = `${show.date}`;
+    concert.appendChild(valueDate);
+
+    const keyVenue = document.createElement("p");
+    keyVenue.classList.add('schedule__key');
+    keyVenue.innerText = 'VENUE';
+    concert.appendChild(keyVenue);
+
+    const valueVenue = document.createElement("p");
+    valueVenue.classList.add('schedule__value');
+    valueVenue.innerText = `${show.venue}`;
+    concert.appendChild(valueVenue);
+
+    const keyLocation = document.createElement("p");
+    keyLocation.classList.add('schedule__key');
+    keyLocation.innerText = 'LOCATION';
+    concert.appendChild(keyLocation);
+
+    const valueLocation = document.createElement("p");
+    valueLocation.classList.add('schedule__value');
+    valueLocation.innerText = `${show.location}`;
+    concert.appendChild(valueLocation);
+
+    const button = document.createElement("button");
+    button.classList.add('schedule__buy-tickets');
+    button.innerText = 'BUY TICKETS';
+    concert.appendChild(button);
+
+    concert.addEventListener('mouseover', function () {
+        concert.classList.add('schedule__concerts--hover');
+    })
+    concert.addEventListener('mouseleave', function () {
+        concert.classList.remove('schedule__concerts--hover');
+    })
+
+    concert.addEventListener('click', function (event) {
+        const activeEl = document.querySelector('.schedule__concerts--active');
+        if (activeEl !== null && activeEl !== concert) {
+            activeEl.classList.remove('schedule__concerts--active');
+        }
+
+        concert.classList.toggle('schedule__concerts--active');
+        event.stopPropagation();
+    })
+
+    schedule.appendChild(concert);
+
+}
+
+
