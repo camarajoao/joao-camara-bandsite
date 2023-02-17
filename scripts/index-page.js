48147c352d07cdbc3fb3c8e7758c768e14df83c6
jcamara <camarajoao1@gmail.com>
2023-02-16 20:30:51 -0800

    start creating conversation section elements

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -16,4 +16,40 @@ const comments = [
         timestamp: "12/20/2020",
         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     }
-]
+];
+
+const conversationSection = document.querySelector('.conversation')
+
+const formContainer = document.createElement('div');
+formContainer.classList.add('conversation__container');
+
+const formContainerLeft = document.createElement('div');
+formContainerLeft.classList.add('conversation__left');
+
+const formIcon = document.createElement('img');
+formIcon.setAttribute("src", "./assets/images/Mohan-muruge.jpg");
+formIcon.classList.add('conversation__icon');
+
+const formContainerRight = document.createElement('div');
+formContainerRight.classList.add('conversation__right');
+
+const form = document.createElement('form');
+form.classList.add('conversation__form');
+
+const formName = document.createElement('p');
+formName.classList.add('form__name');
+formName.innerText: "NAME";
+
+const formNameInput = document.createElement('input');
+formNameInput.classList.add('form__name-input');
+
+
+formContainerLeft.appendChild(formIcon);
+formContainer.appendChild(formContainerLeft);
+
+form.appendChild(formName);
+form.appendChild(formNameInput);
+formContainerRight.appendChild(form);
+formContainer.appendChild(formContainerRight);
+
+conversationSection.appendChild(formContainer);
